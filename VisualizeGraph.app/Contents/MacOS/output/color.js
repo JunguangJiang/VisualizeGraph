42edc16407ef5f7017df7012339b71aa28d25465
JunguangJiang <13395763633@163.com>
2018-01-20 14:57:04 +0800

    删掉了失败的可执行程序

--- VisualizeGraph.app/Contents/MacOS/output/color.js
+++
@@ -1,7 +0,0 @@
-const gray = "#DCDCDC";//灰色
-const skyBlue = "#00BFFF";//天蓝色
-const red = "#FF0000";//红色
-const lightGreen = "#90EE90";//浅绿色
-const purple = "#A020F0";//紫色
-const black ="#000000";//黑色
-const orange = "#FFA500";//橘黄色
